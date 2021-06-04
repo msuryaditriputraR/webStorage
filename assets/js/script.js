6e116fc2fcaa20e7b4bbc088be616eb6a61d8aa5
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2021-06-05 03:42:06 +0800

    set setting prettierrc

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,67 @@
 //inisialiasi variabel untuk menampung elemen dokumen
 const localTotalVictoryField = document.getElementById(
-  "local-total-victory-field"
+    'local-total-victory-field'
 );
 const localMaximumAttempField = document.getElementById(
-  "local-maximum-attemp-field"
+    'local-maximum-attemp-field'
 );
-const destroyDataButton = document.getElementById("destroy-data-button");
-const playButton = document.getElementById("play-button");
-const beforeGameDisplay = document.getElementById("before-game-display");
-const duringGameDisplay = document.getElementById("during-game-display");
-const afterGameDisplay = document.getElementById("after-game-display");
-const answerButton1 = document.getElementById("answer-1-button");
-const answerButton2 = document.getElementById("answer-2-button");
-const answerButton3 = document.getElementById("answer-3-button");
+const destroyDataButton = document.getElementById('destroy-data-button');
+const playButton = document.getElementById('play-button');
+const beforeGameDisplay = document.getElementById('before-game-display');
+const duringGameDisplay = document.getElementById('during-game-display');
+const afterGameDisplay = document.getElementById('after-game-display');
+const answerButton1 = document.getElementById('answer-1-button');
+const answerButton2 = document.getElementById('answer-2-button');
+const answerButton3 = document.getElementById('answer-3-button');
 const sessionUserAnswerField = document.getElementById(
-  "session-user-answer-field"
+    'session-user-answer-field'
 );
 const sessionUserWrongAnswerField = document.getElementById(
-  "session-user-wrong-answer-field"
+    'session-user-wrong-answer-field'
 );
 const sessionTrueAnswerField = document.getElementById(
-  "session-true-answer-field"
+    'session-true-answer-field'
 );
 const sessionUserAttempsField = document.getElementById(
-  "session-user-attemps-amount-field"
+    'session-user-attemps-amount-field'
 );
 
 //inisialisasi fungsi untuk menghasilkan jawaban permainan
 function getAnswer() {
-  let answer = "123".split("");
-  for (let i = 0; i < answer.length; i++) {
-    let j = Math.floor(Math.random() * (i + 1));
-    let tmp = answer[i];
-    answer[i] = answer[j];
-    answer[j] = tmp;
-  }
-  return answer.join("");
+    let answer = '123'.split('');
+    for (let i = 0; i < answer.length; i++) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let tmp = answer[i];
+        answer[i] = answer[j];
+        answer[j] = tmp;
+    }
+    return answer.join('');
 }
 
 //inisialiasi key untuk session storage
-const sessionAnswerKey = "SESSION_ANSWER";
-const sessionUserAttempsKey = "SESSION_USER_ATTEMPS";
-const sessionUserIsPlayingKey = "SESSION_USER_IS_PLAYING";
+const sessionAnswerKey = 'SESSION_ANSWER';
+const sessionUserAttempsKey = 'SESSION_USER_ATTEMPS';
+const sessionUserIsPlayingKey = 'SESSION_USER_IS_PLAYING';
 
 //inisialisasi key untuk local storage
-const localTotalVictoryKey = "LOCAL_TOTAL_VICTORIES_PLAYED";
-const localMaximumAttempsKey = "LOCAL_MAXIMUM_ATTEMPTS";
+const localTotalVictoryKey = 'LOCAL_TOTAL_VICTORIES_PLAYED';
+const localMaximumAttempsKey = 'LOCAL_MAXIMUM_ATTEMPTS';
 
-window.addEventListener("load", () => {
-  if (typeof Storage !== "undefined") {
-    if (sessionStorage.getItem(sessionAnswerKey) === null)
-      sessionStorage.setItem(sessionAnswerKey, "");
-    if (sessionStorage.getItem(sessionUserAttempsKey) === null)
-      sessionStorage.setItem(sessionUserAttempsKey, 0);
-    if (sessionStorage.getItem(sessionUserIsPlayingKey) === null)
-      sessionStorage.setItem(sessionAnswerKey, false);
-    if (sessionStorage.getItem(localTotalVictoryKey) === null)
-      sessionStorage.setItem(localTotalVictoryKey, 0);
-    if (sessionStorage.getItem(localMaximumAttempsKey) === null)
-      sessionStorage.setItem(localMaximumAttempsKey, 0);
-  } else {
-    alert(
-      "Browser anda tidak mendukug web storage, silahkan ganti browser anda"
-    );
-  }
+window.addEventListener('load', () => {
+    if (typeof Storage !== 'undefined') {
+        if (sessionStorage.getItem(sessionAnswerKey) === null)
+            sessionStorage.setItem(sessionAnswerKey, '');
+        if (sessionStorage.getItem(sessionUserAttempsKey) === null)
+            sessionStorage.setItem(sessionUserAttempsKey, 0);
+        if (sessionStorage.getItem(sessionUserIsPlayingKey) === null)
+            sessionStorage.setItem(sessionAnswerKey, false);
+        if (sessionStorage.getItem(localTotalVictoryKey) === null)
+            sessionStorage.setItem(localTotalVictoryKey, 0);
+        if (sessionStorage.getItem(localMaximumAttempsKey) === null)
+            sessionStorage.setItem(localMaximumAttempsKey, 0);
+    } else {
+        alert(
+            'Browser anda tidak mendukug web storage, silahkan ganti browser anda'
+        );
+    }
 });
